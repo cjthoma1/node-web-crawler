0842627388250b4ea22143bb9c20a0760bf70f54
Cray J Thomas Jr <cthomas@buildinmotion.com>
2019-08-27 14:01:44 -0400

    Able to scrape

--- src/index.js
+++ src/index.js
@@ -1,4 +1,26 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-console.log('TEST');
+const url = 'https://news.ycombinator.com';
+const getHTML = async () => {
+    try {
+        let res =  await axios.get(url);
+        getDate(res.data);
+     } catch(err) {
+         console.log('Error', err);
+     }
+}
+
+let getDate = html => {
+    const data = [];
+    const $ = cheerio.load(html);
+    $('table.itemlist tr td:nth-child(3)').each((i, elem) => {
+        data.push({
+            title: $(elem).text(),
+            link: $(elem).find('a.storylink').attr('href')
+        });
+    });
+    console.log('Data', data);
+}
+
+getHTML();
